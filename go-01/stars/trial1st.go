37c65a9f779b7e7e5eeaeb59e69c0a95b28e8b03
Yoonjaeheeeeee <dbswogml0002@gmail.com>
2022-07-24 00:21:29 +0900

    3

--- go-01/stars/trial1st.go
+++ go-01/stars/trial1st.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // var height int = 9
 
-func main() {
+/* func main() {
 	for i := 0; i < 3; i++ {
 		for k := 0; k <= 1-i; k++ {
 			fmt.Print(" ")
@@ -16,4 +16,22 @@ func main() {
 	}
 }
 
-//merge 실험하기
+*/
+
+func main() {
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 1+i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+
+	}
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5-i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+
+	}
+}
