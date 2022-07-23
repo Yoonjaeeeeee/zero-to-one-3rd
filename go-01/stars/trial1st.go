b66c3323ed48504e772490446404968626652fd5
Yoonjaeheeeeee <dbswogml0002@gmail.com>
2022-07-23 23:54:55 +0900

    M

---
+++ go-01/stars/trial1st.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// var height int = 9
+
+func main() {
+	for i := 0; i < 3; i++ {
+		for k := 0; k <= 1-i; k++ {
+			fmt.Print(" ")
+		}
+		for j := 0; j <= 2*i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
